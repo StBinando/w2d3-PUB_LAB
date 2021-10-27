3af05c5e39eac18ae993e7d8545ab991118e866a
Stefano Binando <sbinando@gmail.com>
2021-10-27 11:28:56 +0100

    test test_till_has_cash -- passed

--- tests/pub_test.py
+++ tests/pub_test.py
@@ -4,7 +4,10 @@ from src.pub import Pub
 class PubTest(unittest.TestCase):
 
     def setUp(self):
-        self.pub = Pub("Red Lion", 100.00, [])
+        self.pub = Pub("Red Lion", 100, [])
 
     def test_pub_has_name(self):
         self.assertEqual("Red Lion", self.pub.name)
+
+    def test_till_has_cash(self):
+        self.assertEqual(100, self.pub.till)
