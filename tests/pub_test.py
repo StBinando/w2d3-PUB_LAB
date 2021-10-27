402ab75ea288313300a8919646c541678101fd2a
Stefano Binando <sbinando@gmail.com>
2021-10-27 11:26:19 +0100

    changes drinks attribute in pub test object to list

--- tests/pub_test.py
+++ tests/pub_test.py
@@ -4,7 +4,7 @@ from src.pub import Pub
 class PubTest(unittest.TestCase):
 
     def setUp(self):
-        self.pub = Pub("Red Lion", 100.00, {})
+        self.pub = Pub("Red Lion", 100.00, [])
 
     def test_pub_has_name(self):
         self.assertEqual("Red Lion", self.pub.name)
